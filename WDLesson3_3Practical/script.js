770e3540b53b60fa06642e8fa1246524b2e66af4
Megan780797 <MEganc86@nycstudents.net>
2026-01-06 18:14:34 +0000

    ss

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function dog(){
+ document.getElementById("highlight").innerHTML="Two young dogs";
+}
+
+function cat(){
+    document.getElementById("highlight").innerHTML="A cute cat";
+    }
+    
+function wolf(){
+    document.getElementById("highlight").innerHTML="A grey wolf in the wild";
+    }
+
+function dolphin(){
+    document.getElementById("highlight").innerHTML="A jumping dolphin";
+    }
+
+function chicken(){
+    document.getElementById("highlight").innerHTML="A chicken on grass";
+    }
