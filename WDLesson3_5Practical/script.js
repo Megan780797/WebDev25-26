63a63cbfc9725151b0d950e52769a58b08eef0fb
Megan780797 <MEganc86@nycstudents.net>
2026-01-19 21:27:02 +0000

    ss

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -3,11 +3,17 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-function recArea(){
 
+function recArea(){
+      let len=parseFloat(document.getElementById("l").value);
+      let wid=parseFloat(document.getElementById("w").value);
+      let op=document.getElementById("output");
+      let a=len *wid;   
+      op.innerHTML="AREA = "+a;
 }
 
 function recPerimeter(){
+      let 
 
 }
 
@@ -20,11 +26,15 @@ function recPerimeter(){
 let pi = 3.1415926;
 
 function cirArea(){
-
+      const radius = parseFloat(document.getElementById('radius').value);
+      const area = pi * radius * radius;
+      document.getElementById("output2").innerHTML = "AREA = " + area;
 }
 
 function cirPerimeter(){
-
+      const radius = parseFloat(document.getElementById('radius').value);
+      const perimeter = 2 * pi * radius;
+      document.getElementById("output3").innerHTML = "PERIMETER = " + perimeter;
 }
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
@@ -33,9 +43,16 @@ function cirPerimeter(){
       3) Display the results in the appropriate element
 */
 function triArea(){
-
+      const base = parseFloat(document.getElementById('base').value);
+      const height = parseFloat(document.getElementById('height').value);
+      const area = 0.5 * base * height;
+      document.getElementById("output4").innerHTML = "AREA = " + area;
 }
 
 function triPerimeter(){
-
+      const side1 = parseFloat(document.getElementById('side1').value);
+      const side2 = parseFloat(document.getElementById('side2').value);
+      const side3 = parseFloat(document.getElementById('side3').value);
+      const perimeter = side1 + side2 + side3;
+      document.getElementById("output5").innerHTML = "PERIMETER = " + perimeter;
 }
