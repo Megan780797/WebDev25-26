e0e930f9c8e88c6cc7fc2b553d65b6ff86bba0a4
Megan780797 <MEganc86@nycstudents.net>
2026-01-28 18:12:32 +0000

    ss

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -18,6 +18,15 @@ function calcDist (){
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
+      
+function calcCompInt (){
+      let p= parseFloat(document.GetElementById("p").value);
+      let r= parseFloat(document.GetElementById("r").value)/100;
+      let n= parseFloat(document.GetElementById("n").value);
+      let t= parseFloat(document.GetElementById("t").value);
+      let a= p*Math.pow((1+r/n),(n*t));
+      let op2= document.GetElementById("output2")
+      op2.innerHTML= 'The amount after (${t}) years is (${a.toFixed(2)})';
+}
 
 
