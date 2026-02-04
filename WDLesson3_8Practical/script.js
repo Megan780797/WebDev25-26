c11c0c704ffefe66bbad7c7463f8e8817938fc9d
Megan780797 <MEganc86@nycstudents.net>
2026-02-04 18:19:02 +0000

    ss

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -5,16 +5,17 @@
 /* Challenge 4: Does the function name match the event handler referenced in the event listener? */
 function volume(){
 
-
   /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementById("r").value);
-  let h = parseInt(document.getElementById("h").value);
+  let r = parseInt(document.getElementBy("r").value);
+  let height = parseInt(document.getElementbyId("height").value);
 
   /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output");
+  let output = document.getElementById("output").value;
 
   /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-let v = Math.PI * Math.pow(r,2) * h;
+  let v = math.PI * math.pow(r,2) / h;
+
   /* Challenge 8: Are there any errors in displaying the output? */
- output.innerHTML = `Volume of the cylinder is ${v.toFixed(2)}`;
+  output.innerHTML = `Volume of the cylinder is ${v}`
+
 }
