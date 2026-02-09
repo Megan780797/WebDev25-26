e5f69b212825dc9ae1e5400807240fbc297adc45
Megan780797 <MEganc86@nycstudents.net>
2026-02-09 17:53:19 +0000

    ss

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -13,19 +13,15 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
-function Area(){
-  let length =parseFloat(document.getElementById("length").value);
-   let width=parseFloat(document.getElementById("length").value);
-  let output=document.getElementById("output")
-  let message=""
-
-
-if( rectangle >= 0 ) {
-  message="Inappropriate measurement"
-} else{
-  message="Display the Area"
-}
-output.innerHTML= `Area is ${length * width} ${message}`;
+function area(){
+  let length = document.getElementById("length").value;
+  let width = document.getElementById("width").value;
+  let result = document.getElementById("result");
+  if (l <= 0 || w <= 0) {
+    result.innerHTML = "Inappropriate measurement";
+  } else {
+    result.innerHTML = "Area: " + (length * width);
+  }
 }
 
 /* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
@@ -33,5 +29,16 @@ output.innerHTML= `Area is ${length * width} ${message}`;
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
-
+  let a=document.getElementById("animal").value;
+  let e=document.getElementById("emotion").value;
+  let output=document.getElementById("output");
+  if (a=="dog" && e=="happy"){
+    output.innerHTML=`<img src="dog_happy.png">`;
+  } else if (a=="dog" && e=="sad"){
+    output.innerHTML=`<img src="dog_sad.png">`;
+  } else if (a=="cat" && e=="happy"){
+    output.innerHTML=`<img src="cat_happy.png">`;
+  } else if (a=="cat" && e=="sad"){
+    output.innerHTML=`<img src="cat_sad.png">`;
+  }
 }
