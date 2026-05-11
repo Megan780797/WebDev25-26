9085daaceb57475e97e31ffcc9b4b794242a1e33
Megan780797 <MEganc86@nycstudents.net>
2026-05-11 16:43:25 +0000

    ss

--- WDLesson7_1bPractical/script.js
+++ WDLesson7_1bPractical/script.js
@@ -13,10 +13,15 @@ async function init(){
   let ct = 0;
 
   //Challenge 2: Test the function card() developed in Challenge 1 by displaying only the first violation to the webpage.
-  let fristTicket=data[0];
-  build+=card(fristTicket);
+  //let fristTicket=data[0];
+  //build+=card(fristTicket);
 
   //Challenge 3: Display all the violations to the web page using the function card().
+    for(let i = 0; i < data.length; i++){
+    let Ticket = data[i];
+    build += card(Ticket); // calls the "card" function to build each card
+    ct++;
+  }
 
  
   //result.innerHTML = `${ct} Results found`;
@@ -24,3 +29,12 @@ async function init(){
 }
 
 // Challenge 4: Create a function to filter the information and display only the cards that satisfy specfic condition(s).
+  for(let i = 0; i < data.length; i+=1){
+    let Ticket = data[i];
+    if(Ticket.issue_datee ==issue_date || Ticket.violation== violation){
+      build += card(complaint); // calls the "card" function to build each card
+      ct += 1;
+    }
+  }
+  result.innerHTML = `${ct} Results found`;
+  output.innerHTML = build;  
